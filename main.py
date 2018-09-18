d28a934cf5b6432b67e7860fe964864a18d695a8
WrenVin <31417492+WrenVin@users.noreply.github.com>
2018-09-18 08:03:26 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from time import sleep
+def collatz(num):
+    step = 0
+    global numlist
+    numlist = []
+    print(num)
+    while num != 1:
+       if num < 1:
+           print('error')
+           break
+       if num % 2 == 0:
+           num /= 2
+           numlist.append(num)
+           step += 1
+       else:
+           num = num*3+1
+           numlist.append(num)
+           step += 1
+    return step
+
+while True:
+    inn = int(input('What number would you like to enter?:'))
+    steps = collatz(inn)
+    print('It took {} steps for {} to be reduced to one!'.format(steps, inn))
+    inn = input('would you like to see the steps individually?(y/n):')
+    if inn == 'y':
+        for step in numlist:
+            print(step)
+            sleep(0.1)
